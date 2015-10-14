9bcf0c53b7b85959dc669a37b51f0844ea71c734
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-10-14 14:37:19 -0400

    remove unique index on bucket_lists title

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011232420) do
+ActiveRecord::Schema.define(version: 20151014183232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,6 @@ ActiveRecord::Schema.define(version: 20151011232420) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id",     null: false
   end
-
-  add_index "bucket_lists", ["title"], name: "index_bucket_lists_on_title", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                            null: false
