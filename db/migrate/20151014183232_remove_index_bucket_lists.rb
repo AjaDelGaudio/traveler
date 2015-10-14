4a9a85a504d8214ebe9fd248e9a211943204dd77
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-10-14 14:40:25 -0400

    fix hound error

--- db/migrate/20151014183232_remove_index_bucket_lists.rb
+++ db/migrate/20151014183232_remove_index_bucket_lists.rb
@@ -2,6 +2,7 @@ class RemoveIndexBucketLists < ActiveRecord::Migration
   def up
     remove_index :bucket_lists, :title
   end
+  
   def down
     add_index :bucket_lists, :title, unique: true
   end
