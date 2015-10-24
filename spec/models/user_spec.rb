c764d580f6a46e7a2b9aac5e09f702d23cbeea85
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-10-24 14:36:08 -0400

    fix hound errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,12 @@ describe User do
   it { should have_many(:adventures).through(:adventure_users) }
   it { should have_many(:bucket_lists) }
 
-  it { should have_valid(:username).when('useruser', 'iamauser12345') }
-  it { should_not have_valid(:username).when(nil, '') }
+  it { should have_valid(:username).when("useruser", "iamauser12345") }
+  it { should_not have_valid(:username).when(nil, " ") }
 
   it { should validate_uniqueness_of(:username) }
-  
-  it { should have_valid(:email).when('user@mailhere.com', 'thisuser@mail.com') }
-  it { should_not have_valid(:email).when(nil, '', 'user', '@', 'usermail.com', 'user@mail') }
+
+  it { should have_valid(:email).when("user@mailhere.com", "thisuser@mail.com") }
+  it { should_not have_valid(:email)
+    .when(nil, " ", "user", "@", "usermail.com", "user@mail") }
 end
