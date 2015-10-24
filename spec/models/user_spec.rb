ebc9ca982e261dfbc40f2d29448cb47cf86e9f2b
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-10-24 14:25:12 -0400

    fix user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe User do
-  # subject { FactoryGirl.build(:user) }
+  subject { FactoryGirl.build(:user) }
+  it { should have_many(:adventures).through(:adventure_users) }
   it { should have_many(:bucket_lists) }
-  it { should have_many(:adventures) }
 
+  it { should have_valid(:username).when('useruser', 'iamauser12345') }
+  it { should_not have_valid(:username).when(nil, '') }
 
-  # it { should have_valid(:username).when('useruser', 'iamauser12345') }
-  # it { should_not have_valid(:username).when(nil, '') }
-  #
-  # it { should validate_uniqueness_of(:username) }
-  #
-  # it { should have_valid(:email).when('user@mailhere.com', 'thisuser@mail.com') }
-  # it { should_not have_valid(:email).when(nil, '', 'user', '@', 'usermail.com', 'user@mail') }
+  it { should validate_uniqueness_of(:username) }
+  
+  it { should have_valid(:email).when('user@mailhere.com', 'thisuser@mail.com') }
+  it { should_not have_valid(:email).when(nil, '', 'user', '@', 'usermail.com', 'user@mail') }
 end
