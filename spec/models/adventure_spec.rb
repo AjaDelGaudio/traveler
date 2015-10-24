332ba83cc23e71b43a8a517e6f8f9cd2a07f833d
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-10-24 00:47:36 -0400

    fix hound errors

--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -7,26 +7,19 @@ describe Adventure do
   it { should have_many(:users) }
 
   it { should have_valid(:name).when("Visit Sue", "Eat alligator") }
-  it { should have_valid(:address).when(
-    'Paris, France',
-    '33 Harrison Ave, Boston'
-  ) }
+  it { should have_valid(:address).when("Paris, France", "1 A St., NYC") }
 
   it { should have_valid(:latitude).when(40.7127837, 39.904211) }
   it { should_not have_valid(:latitude).when("cat", ".") }
-  it { should validate_numericality_of(:latitude).is_greater_than_or_equal_to (
-    -180
-  ) }
-  it { should validate_numericality_of(:latitude).is_less_than_or_equal_to (
-    180
-  ) }
+  it { should validate_numericality_of(:latitude)
+    .is_greater_than_or_equal_to(-180) }
+  it { should validate_numericality_of(:latitude)
+    .is_less_than_or_equal_to(180) }
 
   it { should have_valid(:longitude).when(116.4073949999, 116.4073949999) }
   it { should_not have_valid(:longitude).when("cat", ".") }
-  it { should validate_numericality_of(:longitude).is_greater_than_or_equal_to (
-    -180
-  ) }
-  it { should validate_numericality_of(:longitude).is_less_than_or_equal_to (
-    180
-  ) }
+  it { should validate_numericality_of(:longitude)
+    .is_greater_than_or_equal_to(-180) }
+  it { should validate_numericality_of(:longitude)
+    .is_less_than_or_equal_to(180) }
 end
