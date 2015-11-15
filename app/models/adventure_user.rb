d6c7cc041b88e63ce07e67206076f97612585824
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-11-14 22:46:46 -0500

    add unit test and presence of and numericality validations

--- app/models/adventure_user.rb
+++ app/models/adventure_user.rb
@@ -1,4 +1,10 @@
 class AdventureUser < ActiveRecord::Base
   belongs_to :adventure
   belongs_to :user
+
+  validates :adventure, presence: true
+  validates :adventure, numericality: { only_integer: true }
+
+  validates :user, presence: true
+  validates :user, numericality: { only_integer: true }
 end
---
+++ spec/models/adventure_user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Adventure do
+  subject { FactoryGirl.build(:adventure_user) }
+
+  it { should belong_to(:adventure) }
+  it { should belong_to(:user) }
+end
