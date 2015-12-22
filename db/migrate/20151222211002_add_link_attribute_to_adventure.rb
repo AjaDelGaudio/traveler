587217f59dce20eb24e0c008154ddf6452e6f05d
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-22 16:24:12 -0500

    add link to adventure migration up/down methods

--- db/migrate/20151222211002_add_link_attribute_to_adventure.rb
+++ db/migrate/20151222211002_add_link_attribute_to_adventure.rb
@@ -1,4 +1,8 @@
 class AddLinkAttributeToAdventure < ActiveRecord::Migration
-  def change
+  def up
+    add_column :adventures, :link, :string
+  end
+  def down
+    remove_column :adventures, :link
   end
 end
