6c98e6856736386ff4b94f1673ed61a9297e09cf
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-22 16:59:59 -0500

    fix hound error

--- db/migrate/20151222211002_add_link_attribute_to_adventure.rb
+++ db/migrate/20151222211002_add_link_attribute_to_adventure.rb
@@ -2,7 +2,7 @@ class AddLinkAttributeToAdventure < ActiveRecord::Migration
   def up
     add_column :adventures, :link, :string
   end
-  
+
   def down
     remove_column :adventures, :link
   end
