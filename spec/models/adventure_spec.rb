4eb4dcb81cf15442bff58c604eac5cb583a951ae
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 14:06:59 -0500

    migrate add is_shared attribute to adventures

--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -9,6 +9,10 @@ describe Adventure do
   it { should have_many :bucket_list_adventures }
   it { should have_many(:bucket_lists).through(:bucket_list_adventures) }
 
+  # it { should validate_presence_of(:link) }
+  # it { should have_valid(:link).when("www.google.com", nil) }
+  # it { should_not have_valid(:link).when("google", "www.") }
+
   it { should validate_inclusion_of(:is_shared).in_array([true, false]) }
 
   # it { should have_valid(:latitude).when(40.7127837, nil) }
