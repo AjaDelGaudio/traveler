71ec5f0b789ea76c028f825d6f4f7ffcad0eabc9
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 17:00:09 -0500

    migrate drop table adventure_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224204335) do
+ActiveRecord::Schema.define(version: 20151224215723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "adventure_users", force: :cascade do |t|
-    t.integer  "adventure_id", null: false
-    t.integer  "user_id",      null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
