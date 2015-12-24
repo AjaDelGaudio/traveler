230814c57a6b282f4343122fd984611f00944f13
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 17:26:14 -0500

    create migration for add user_id foreign key to adventures table

--- db/migrate/20151224220416_add_user_association_to_adventures.rb
+++
@@ -1,9 +0,0 @@
-class AddUserAssociationToAdventures < ActiveRecord::Migration
-  def up
-    add_column :adventures, :user, :belongs_to
-  end
-
-  def down
-    remove_column :adventures, :user
-  end
-end
