a1e53318da22b037289939304b045a092e7947fc
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 15:39:29 -0500

    correct table to adventrues in migration

--- db/migrate/20151224203554_add_is_achieved_to_adventures.rb
+++ db/migrate/20151224203554_add_is_achieved_to_adventures.rb
@@ -1,9 +1,9 @@
 class AddIsAchievedToAdventures < ActiveRecord::Migration
   def up
-    add_column :bucket_list_adventures, :is_achieved, :boolean, default: false, null: false
+    add_column :adventures, :is_achieved, :boolean, default: false, null: false
   end
 
   def down
-    remove_column :bucket_list_adventures, :is_achieved
+    remove_column :adventures, :is_achieved
   end
 end
