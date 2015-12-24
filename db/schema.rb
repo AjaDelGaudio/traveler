efdb8f7e82fa6315a4fa6df6a475473b21c1a1c4
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 15:40:42 -0500

    migrate add is_achieved to advetnrues w/ correct table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224203058) do
+ActiveRecord::Schema.define(version: 20151224203554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20151224203058) do
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "link"
+    t.boolean  "is_achieved", default: false, null: false
   end
 
   create_table "bucket_list_adventures", force: :cascade do |t|
