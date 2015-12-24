bf4374ee52a2c3794e1acdbc74cf99b4c4b4b1fe
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 15:16:07 -0500

    create up/down methods for migration to remove is_achieved from bucket_list_adventures

--- db/migrate/20151224201339_drop_is_achieved_from_bucket_list_adventures.rb
+++ db/migrate/20151224201339_drop_is_achieved_from_bucket_list_adventures.rb
@@ -1,4 +1,9 @@
 class DropIsAchievedFromBucketListAdventures < ActiveRecord::Migration
-  def change
+  def up
+    remove_column :bucket_list_adventures, :is_achieved
+  end
+
+  def down
+    add_column :bucket_list_adventures, :is_achieved, :boolean, default: false, null: false
   end
 end
