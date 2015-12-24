724c8003799c7a4caa604ce251e363bc294843c8
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 17:28:03 -0500

    fix down method on migration

--- db/migrate/20151224222006_add_user_id_to_adventures.rb
+++ db/migrate/20151224222006_add_user_id_to_adventures.rb
@@ -2,8 +2,8 @@ class AddUserIdToAdventures < ActiveRecord::Migration
   def up
     add_reference :adventures, :user, index: true, foreign_key: true
   end
-  
+
   def down
-    remove_reference :products, :user, index: true, foreign_key: true
+    remove_reference :adventures, :user, index: true, foreign_key: true
   end
 end
