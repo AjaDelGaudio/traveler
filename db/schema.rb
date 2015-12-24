dc0f4109ef6ef16758b5658bf9248cdcf3afa523
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 15:42:59 -0500

    migrate add is_shared to adventures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224203554) do
+ActiveRecord::Schema.define(version: 20151224204103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151224203554) do
     t.datetime "updated_at",                  null: false
     t.string   "link"
     t.boolean  "is_achieved", default: false, null: false
+    t.boolean  "is_shared",   default: false, null: false
   end
 
   create_table "bucket_list_adventures", force: :cascade do |t|
