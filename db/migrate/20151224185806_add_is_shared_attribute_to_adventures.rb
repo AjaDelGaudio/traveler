17e89561405ae47c12e58c49f59dc139dd5b1b72
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-24 14:00:01 -0500

    create migration add is_shared to adventures model w/ up/down methods

---
+++ db/migrate/20151224185806_add_is_shared_attribute_to_adventures.rb
@@ -0,0 +1,9 @@
+class AddIsSharedAttributeToAdventures < ActiveRecord::Migration
+  def up
+    add_column :adventures, :is_shared, :boolean, default: false, null: false
+  end
+
+  def down
+    remove_column :adventures, :is_shared
+  end
+end
