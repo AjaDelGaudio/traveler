35819b0ccdb745aca93e420a197370e2a915addb
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-23 22:13:58 -0500

    add select bucket list to sign_in_with_adventures method in user_sign_in_helper

--- spec/support/user_sign_in_helper.rb
+++ spec/support/user_sign_in_helper.rb
@@ -22,16 +22,18 @@ module UserSignIn
     fill_in "Title", with: "First Bucket List"
     click_button "Save It!"
 
+    visit new_adventure_path
+    fill_in "Name", with: "Go pearl diving"
+    select "First Bucket List", from: "Bucket list"
+    click_button "Toss it in!"
+
     visit new_bucket_list_path
     fill_in "Title", with: "Second Bucket List"
     click_button "Save It!"
 
     visit new_adventure_path
-    fill_in "Name", with: "Go pearl diving"
-    click_button "Toss it in!"
-
-    visit new_adventure_path
     fill_in "Name", with: "Paint something"
+    select "Second Bucket List", from: "Bucket list"
     click_button "Toss it in!"
   end
 end
