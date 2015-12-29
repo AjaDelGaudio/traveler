92c3dc5cffdfadc4a697e8d4be73d8af543ccf96
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-28 20:09:46 -0500

    fix hound errors: double quoted strings

--- spec/features/adventures/user_edits_adventure_spec.rb
+++ spec/features/adventures/user_edits_adventure_spec.rb
@@ -43,9 +43,9 @@ feature "user edits adventure", %(
   scenario "authenticated user successfully edits an adventure's bucket list" do
     user = FactoryGirl.create(:user)
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
     adventure = FactoryGirl.create(:adventure, user_id: user.id)
     bucket_list_1 = FactoryGirl.create(:bucket_list, user_id: user.id)
     bucket_list_2 = FactoryGirl.create(
