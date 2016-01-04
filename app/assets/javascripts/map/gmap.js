8261e53ee508534bf364368224f508ed266a3de4
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-03 19:41:08 -0500

    add script building map to adventures show view

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,13 +1,13 @@
 var geocoder;
 // var address = "San Diego, CA";
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map-container"), {
-    center: {lat: 43.825592, lng: 87.616848},
-    zoom: 2,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  });
-
-  map = new google.maps.Map(document.getElementById("map-container"),mapProp);
-
-}
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById("map-container"), {
+//     center: {lat: 43.825592, lng: 87.616848},
+//     zoom: 2,
+//     mapTypeId: google.maps.MapTypeId.HYBRID
+//   });
+//
+//   map = new google.maps.Map(document.getElementById("map-container"),mapProp);
+//
+// }
