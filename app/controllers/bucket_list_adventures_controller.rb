21ccd47cd202567a1322caf09f798268a830de82
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-09 23:54:19 -0500

    fix hound errors

--- app/controllers/bucket_list_adventures_controller.rb
+++ app/controllers/bucket_list_adventures_controller.rb
@@ -1,8 +1,6 @@
 class BucketListAdventuresController < ApplicationController
   def update
-    binding.pry
     @bucket_list_adventure = BucketListAdventure.find(params[:id])
-    binding.pry
     @bucket_list_adventure.save
   end
 
