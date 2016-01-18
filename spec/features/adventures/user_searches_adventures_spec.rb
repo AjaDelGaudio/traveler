b532a5e8c582bbb3ec73267785a08e00f7f40608
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-18 17:32:40 -0500

    create feature test user story and acceptance criteria

---
+++ spec/features/adventures/user_searches_adventures_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature "user searches adventures", %(
+  As a last minute traveler or traveler who's stay is unexpectedly extended,
+  I want to quickly find an adventure
+  So that I can make plans with haste.
+) do
+  # Acceptance Criteria
+  # [ ] I must be signed in to find my private adventures
+  # [ ] I can find any public adventure shared by other users or myself
+  # [ ] I can find adventures by browsing a list of public adventures
+  # [ ] If there are no relevant adventures, I am informed
+  # [ ] I can find adventures by typing key words in a search bar
+  # [ ] Using the search bar will return a list of relevant adventures
+
+  
+
+end
