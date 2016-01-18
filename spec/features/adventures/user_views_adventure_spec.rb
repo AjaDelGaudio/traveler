ea19dfde117018a84b1aabdf8d58ebe0df4ee91b
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-18 16:01:46 -0500

    update user views adventures feature test for reflect new route path

--- spec/features/adventures/user_views_adventure_spec.rb
+++ spec/features/adventures/user_views_adventure_spec.rb
@@ -66,7 +66,8 @@ feature "user views their adventures", %(
       is_public: false
     )
     adventure = FactoryGirl.create(
-      :adventure
+      :adventure,
+      notes: "do all the things"
     )
     bucket_list_adventure = FactoryGirl.create(
       :bucket_list_adventure,
@@ -74,11 +75,11 @@ feature "user views their adventures", %(
       adventure_id: adventure.id
     )
 
-    visit root_path
+    visit adventures_path
 
     expect(page).to have_content("You can do that after you sign in or sign up!")
     expect(page).to have_content("Log in")
-    expect(page).not_to have_content(bucket_list.description)
+    expect(page).not_to have_content(adventure.notes)
   end
 
   scenario "authenticated user successfully views list of their adventures" \
