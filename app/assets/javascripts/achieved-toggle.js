49012e59069a7ca44677382de72466d49d736ff8
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-18 21:06:03 -0500

    update template to reflect earlier database changes

--- app/assets/javascripts/achieved-toggle.js
+++ app/assets/javascripts/achieved-toggle.js
@@ -1,17 +1,28 @@
-$(".achieved-toggle").click(function(e){
-  e.preventDefault();
+// <% if adventure.is_achieved %>
+//   <%= link_to (fa_icon "check-square-o") + " Seen it! Done it!",
+//     "#", class: "achieved-toggle", id: bla.id %>
+// <% else %>
+//   <%= link_to (fa_icon "square-o") + " Seen it! Done it!",
+//     "#", class: "achieved-toggle", id: bla.id %>
+// <% end %>
 
-  var id = e.target.id;
-  var updatePath = "/adventures/" + id;
 
-  $.ajax({
-    method: "PATCH",
-    url: updatePath,
-  });
 
-  if ($(this).html() == '<i class="fa fa-square-o"></i> Seen it! Done it!') {
-    $(this).html('<i class="fa fa-check-square-o"></i> Seen it! Done it!');
-  } else {
-    $(this).html('<i class="fa fa-square-o"></i> Seen it! Done it!');
-  }
-});
+
+// $(".achieved-toggle").click(function(e){
+//   e.preventDefault();
+//
+//   var id = e.target.id;
+//   var updatePath = "/adventures/" + id;
+//
+//   $.ajax({
+//     method: "PATCH",
+//     url: updatePath,
+//   });
+//
+//   if ($(this).html() == '<i class="fa fa-square-o"></i> Seen it! Done it!') {
+//     $(this).html('<i class="fa fa-check-square-o"></i> Seen it! Done it!');
+//   } else {
+//     $(this).html('<i class="fa fa-square-o"></i> Seen it! Done it!');
+//   }
+// });
