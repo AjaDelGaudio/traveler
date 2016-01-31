49093e683754a9c9da7025b42484a8b2f05448ec
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-01-31 18:55:08 -0500

    fix column name in remove links column from adventures migration

--- db/migrate/20160129151833_remove_links_column_from_adventures.rb
+++ db/migrate/20160129151833_remove_links_column_from_adventures.rb
@@ -1,9 +1,9 @@
 class RemoveLinksColumnFromAdventures < ActiveRecord::Migration
   def up
-    remove_column :adventures, :links
+    remove_column :adventures, :link
   end
 
   def down
-     add_column :adventures, :links
+     add_column :adventures, :link
   end
 end
