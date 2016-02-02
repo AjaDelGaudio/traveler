beade22505836290e1fa3ba599d93975206dbcc5
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-01 22:08:37 -0500

    mend

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,5 @@
 class Link < ActiveRecord::Base
   belongs_to :adventure
 
-  it { should validate_presence_of(:link_address) }
-  it { should have_valid(:link_address).when("www.wikipedia.com", "https://www.google.com") }
-  it { should_not have_valid(:link_address).when(nil, " ") }
+
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 describe Adventure do
   subject { FactoryGirl.build(:link) }
+
   it { should belong_to(:adventure) }
 
+  it { should validate_presence_of(:link_address) }
+  it { should have_valid(:link_address).when("www.wikipedia.com", "https://www.google.com") }
+  it { should_not have_valid(:link_address).when(nil, " ") }
 end
