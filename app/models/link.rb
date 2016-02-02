1eabd00052c195bc78867964d26f0891b270cf54
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-01 22:05:11 -0500

    add validations and belongs_to tests to link unit test

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ActiveRecord::Base
   belongs_to :adventure
 
+  it { should validate_presence_of(:link_address) }
+  it { should have_valid(:link_address).when("www.wikipedia.com", "https://www.google.com") }
+  it { should_not have_valid(:link_address).when(nil, " ") }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
 describe Adventure do
+  subject { FactoryGirl.build(:link) }
+  it { should belong_to(:adventure) }
 
 end
