ecd1a9285ed03136c5e4cb34edee38c2a70bf165
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-02 19:10:38 -0500

    create-links-feature-test

---
+++ spec/features/links/user_creates_new_links_spec.rb
@@ -0,0 +1,14 @@
+feature "user creates a link", %(
+  As a last minute traveler or traveler who's stay is unexpectedly extended,
+  I want to add a link
+  So I can add it while creating an adventure.
+) do
+  # Acceptance Criteria
+  # [] I can add multiple links to an adventure
+  # [] If I add a link, I must submit a link address
+  # [] If I add a link, I may also add a link name
+  # [] I can click a button to add a new link
+
+
+
+end
