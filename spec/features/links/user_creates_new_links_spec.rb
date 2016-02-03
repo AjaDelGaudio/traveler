27dfcd6f86efad6d3f6c4fe92e792996b111397b
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-02 19:26:44 -0500

    write feature test for authenticated user sucessfully adds a link to an adventure

--- spec/features/links/user_creates_new_links_spec.rb
+++ spec/features/links/user_creates_new_links_spec.rb
@@ -9,6 +9,21 @@ feature "user creates a link", %(
   # [] If I add a link, I may also add a link name
   # [] I can click a button to add a new link
 
+  scenario "authenticated user successfully adds a link to an adventure" do
+    user = FactoryGirl.create(:user)
+    bucket_list = FactoryGirl.create(:bucket_list, user_id: user.id)
+    sign_in(user)
 
+    visit new_adventure_path
+    fill_in "Adventure address:", with: "Egypt"
+    click_button "Add link"
 
+    fill_in "Link name:", with: "Wikipedia"
+    fill_in "Link address:", with "www.wikipedia.org"
+    click_button "Save link"
+    click_button "Save it!"
+
+    expect(page).to have_content("Excellent! Another adventure awaits!")
+    expect(page).not_to have_content("Address can't be blank")
+  end
 end
