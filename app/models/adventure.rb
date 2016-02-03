abc863af3fc210fe769c510a4db1122a23561d03
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-03 17:38:59 -0500

    mend

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -8,8 +8,7 @@ class Adventure < ActiveRecord::Base
   has_many :links
 
   accepts_nested_attributes_for :bucket_list_adventures, allow_destroy: true
-  accepts_nested_attributes_for :bucket_lists
-  accepts_nested_attributes_for :links
+  accepts_nested_attributes_for :bucket_lists, :links
 
   validates :user_id, presence: true
   validates :user_id, numericality: { only_integer: true }
