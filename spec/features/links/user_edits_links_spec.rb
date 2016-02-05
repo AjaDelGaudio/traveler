a6a7803be9b8fd465342c86c00153f43d85d0803
Aja DelGaudio <ajadelgaudio@gmail.com>
2016-02-04 23:41:27 -0500

    mend

--- spec/features/links/user_edits_links_spec.rb
+++ spec/features/links/user_edits_links_spec.rb
@@ -10,7 +10,38 @@ feature "user edits a link", %(
   # [] If I add a link, I may also add a link name
   # [] I can click a button to add a new link
 
-  scenario "authenticated user successfully adds a link to an adventure" do
+  scenario "authenticated user successfully adds another link to an adventure" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    bucket_list = FactoryGirl.create(
+      :bucket_list,
+      user_id: user.id
+    )
+    adventure = FactoryGirl.create(:adventure, user_id: user.id)
+    bucket_list_adventure = FactoryGirl.create(
+      :bucket_list_adventure,
+      adventure_id: adventure.id,
+      bucket_list_id: bucket_list.id
+    )
+    link = FactoryGirl.create(:link, user_id: user.id, adventure_id: adventure.id)
+
+    visit edit_adventure_path(adventure)
+    click_button "Add link"
+
+    link_name_fields = page.all("Link name:")
+    fill_in link_name_fields[1], with: "Wikitravel - Jinja"
+
+    link_address_fields = page.all("Link address:")
+    fill_in link_address_fields[1], with: "wikitravel.org/en/Jinja"
+
+    click_button "Save It!"
+
+    expect(page).to have_content("Changes saved!")
+    expect(page).not_to have_content("Address can't be blank")
+    expect(page).not_to have_content("Link address can't be blank")
+  end
+
+  scenario "authenticated user successfully edits a link to an adventure" do
     user = FactoryGirl.create(:user)
     sign_in(user)
     bucket_list = FactoryGirl.create(
@@ -28,7 +59,7 @@ feature "user edits a link", %(
 
     visit edit_adventure_path(adventure)
     click_button "Add link"
-    
+
     link_name_fields = page.all("Link name:")
     fill_in link_name_fields[1], with: "Wikitravel - Jinja"
 
